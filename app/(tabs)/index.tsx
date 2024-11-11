5b1bb761ecb2ea063b4205d03d6b9ff586112543
Peyush Gedela <gedela.peyush2022@vitstudent.ac.in>
2024-11-12 00:21:37 +0530

    pey-first

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import axios from "axios";
+import BreakingNews from "@/components/BreakingNews";
 
-type Props = {}
+type Props = {};
 
 const Page = (props: Props) => {
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
-  )
-}
+  const [Breakingnews, setBreakingnews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default Page
+  useEffect(() => {
+    getBreakingNews();
+  }, []);
+
+  const getBreakingNews = async () => {
+    try {
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_NEWS_API}&country=in&language=en&image=1&removeduplicate=1&size=5`;
+      const response = await axios.get(URL);
+      if (response && response.data) {
+        setBreakingnews(response.data.results);
+        setIsLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Home</Text>
+        <Ionicons name="notifications-outline" size={24} color="black" />
+      </View>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <BreakingNews newslist={Breakingnews} />
+      )}
+    </SafeAreaView>
+  );
+};
+
+export default Page;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 16,
     alignItems: "center",
   },
-})
+  title: {
+    fontSize: 24,
+    fontWeight: "900",
+  },
+  breakingnews: {},
+});
