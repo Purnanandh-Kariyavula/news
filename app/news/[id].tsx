117bfbd75052aaf468e8c19911ac5f2a5688ca87
Purnanandh-Kariyavula <purnanandhkariyavula2004@gmail.com>
2024-11-12 14:26:17 +0530

    safe

--- app/news/[id].tsx
+++ app/news/[id].tsx
@@ -7,6 +7,7 @@ import { Linking, TouchableOpacity } from "react-native";
 import { Colors } from "@/constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage"; // Import AsyncStorage
+import { SafeAreaView } from "react-native-safe-area-context";
 
 type Props = {};
 
@@ -83,50 +84,52 @@ const NewsDetails = (props: Props) => {
           headerShown: false,
         }}
       />
-      <ScrollView contentContainerStyle={styles.container}>
-        <Image source={{ uri: news.image_url }} style={styles.image} />
-        <View style={styles.contentContainer}>
-          <Text style={styles.title}>{news.title}</Text>
-          <Text style={styles.description}>{news.description}</Text>
+      <SafeAreaView>
+        <ScrollView contentContainerStyle={styles.container}>
+          <Image source={{ uri: news.image_url }} style={styles.image} />
+          <View style={styles.contentContainer}>
+            <Text style={styles.title}>{news.title}</Text>
+            <Text style={styles.description}>{news.description}</Text>
 
-          <View style={styles.metaContainer}>
-            <Text style={styles.metaText}>Source: {news.source_name}</Text>
-            <Text style={styles.metaText}>
-              Published on: {new Date(news.pubDate).toLocaleDateString()}
-            </Text>
-            <Text style={styles.metaText}>
-              Keywords:{" "}
-              {news.keywords?.length
-                ? news.keywords.join(", ")
-                : "No keywords available"}
+            <View style={styles.metaContainer}>
+              <Text style={styles.metaText}>Source: {news.source_name}</Text>
+              <Text style={styles.metaText}>
+                Published on: {new Date(news.pubDate).toLocaleDateString()}
+              </Text>
+              <Text style={styles.metaText}>
+                Keywords:{" "}
+                {news.keywords?.length
+                  ? news.keywords.join(", ")
+                  : "No keywords available"}
+              </Text>
+            </View>
+            <Text style={styles.link}>
+              <TouchableOpacity
+                onPress={() => Linking.openURL(news.link)}
+                style={styles.fullArticleButton}
+              >
+                <Text style={styles.linkText}>Read Full Article Here</Text>
+              </TouchableOpacity>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  onPress={() => {
+                    bookmarkNews ? removeBookmark(news.article_id) : saveBookmark(news.article_id);
+                  }}
+                  style={[styles.iconButton, styles.bookmarkButton]}
+                >
+                  <Ionicons name={bookmarkNews ? "bookmark" : "bookmark-outline"} size={24} color={bookmarkNews ? Colors.tint : Colors.blue} />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => bookmarkNews ? removeBookmark(news.article_id) : saveBookmark(news.article_id)}
+                  style={[styles.iconButton, styles.likeButton]}
+                >
+                  <Ionicons name={bookmarkNews ? "heart" : "heart-outline"} size={24} color={bookmarkNews ? Colors.tint : Colors.blue} />
+                </TouchableOpacity>
+              </View>
             </Text>
           </View>
-          <Text style={styles.link}>
-            <TouchableOpacity
-              onPress={() => Linking.openURL(news.link)}
-              style={styles.fullArticleButton}
-            >
-              <Text style={styles.linkText}>Read Full Article Here</Text>
-            </TouchableOpacity>
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity
-                onPress={() => {
-                  bookmarkNews ? removeBookmark(news.article_id) : saveBookmark(news.article_id);
-                }}
-                style={[styles.iconButton, styles.bookmarkButton]}
-              >
-                <Ionicons name={bookmarkNews ? "bookmark" : "bookmark-outline"} size={24} color={bookmarkNews ? Colors.tint : Colors.blue} />
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => bookmarkNews ? removeBookmark(news.article_id) : saveBookmark(news.article_id)}
-                style={[styles.iconButton, styles.likeButton]}
-              >
-                <Ionicons name={bookmarkNews ? "heart" : "heart-outline"} size={24} color={bookmarkNews ? Colors.tint : Colors.blue} />
-              </TouchableOpacity>
-            </View>
-          </Text>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 };
